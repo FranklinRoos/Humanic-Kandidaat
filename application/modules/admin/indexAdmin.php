6adb2e80780b3617c5f8f9f314db1e46c2f30895
Franklin Roos <frankieboy37@hotmail.com>
2016-08-15 20:55:55 +0200

    Update indexAdmin.php

--- application/modules/admin/indexAdmin.php
+++ application/modules/admin/indexAdmin.php
@@ -36,21 +36,12 @@ if(isset($_SESSION['loginnaam']))
         //navigatie();
         fFooter();                                // ALLE NAVIGATIE FUNCTIES BEVINDEN ZICH IN: application/config/default_functions.php
     }
-    if( $_SESSION["user_authorisatie"]=='ptr') //ik had een user-autorisatie 'ptr'  voor pieter bedacht , omdat ik de functies bij user-autorisatie 'admin' 
-    {                                                            // als oefening gebied heb gebruikt. 
-    navigatiePtr();   // pieter krijgt dan een aangepaste navigatie balk te zien waarin niet de experimentele functies zijn opgenomen zoals in de  navigatieAdmin()
-    echo "<br><br><br>"; 
-    echo "<h2>".$_SESSION['loginnaam'].",je bent nu ingelogd als Administrator!<br />Ga verder door een keuze te maken uit de navigatie.</h2>";
-    echo "<input type =\"button\" onclick=\"inofuitLoggen(0)\" value=\"Uitloggen\" class=\"btn\""
-        . " style=\"padding: 2px 20px; margin-top: -2px; border: 1px solid rgba(0, 0, 0, 0.33);\"></button>";
-    fFooter();
-    }
   }   
  elseif(!isset($_SESSION['loginnaam'])) 
      {
      
      echo "<br /><div class=\"bericht\"><h2 align=center>Dit is de website van HumanIC</h2>";
-     echo "<br/><h3>U heeft niet de juiste rechten</h3>";
+     echo "<br/><h3>U heeft niet de juiste rechten of bent nog niet ingelogd</h3>";
     //echo "<br /><h3>U dient eerst<a href=\"../humanic-portal/login.php\"> ingelogd</a> te zijn</h3></div>";
     fFooter();
      
